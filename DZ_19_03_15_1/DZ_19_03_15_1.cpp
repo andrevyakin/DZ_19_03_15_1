069daee68e1acf348fc768a38b0e73aa559ee587
andrevyakin <andrevyakin@gmail.com>
2015-03-24 09:26:01 +0300

    WinMain

---
+++ DZ_19_03_15_1/DZ_19_03_15_1.cpp
@@ -0,0 +1,72 @@
+#include<Windows.h>
+#include<stdlib.h>
+#include<string>
+#include<tchar.h>
+
+static TCHAR WindowsClass[] = _T("win32app");
+
+static TCHAR Title[] = _T("MyApp");
+
+HINSTANCE hinst;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hinstance,
+	HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine,
+	int nCmdShow)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hinstance;
+	wcex.hIcon = LoadIcon(hinstance, MAKEINTRESOURCE(IDI_APPLICATION));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	/*wcex.hbrBackground = CreateSolidBrush(RGB(100, 70, 150));*/
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = WindowsClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
+
+	if (!RegisterClassEx(&wcex))
+	{
+		MessageBox(NULL, _T("Call faild!"), _T("MyApp"), NULL);
+		return 1;
+	}
+
+	hinst = hinstance;
+
+	HWND hWnd = CreateWindow(
+		WindowsClass,
+		Title,
+		WS_OVERLAPPEDWINDOW,
+		100,
+		100,
+		500,
+		500,
+		NULL,
+		NULL,
+		hinst,
+		NULL);
+
+	if (!hWnd)
+	{
+		MessageBox(NULL, _T("Create window faild!"), _T("MyApp"), NULL);
+		return 1;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return(int)msg.wParam;
+}
